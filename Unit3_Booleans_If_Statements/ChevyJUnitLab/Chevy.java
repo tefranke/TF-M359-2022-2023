619279392b64ea83d279968de8fdfd4f44a90c22
Tyler Franke <tefranke@icloud.com>
2022-09-29 11:59:43 -0500

    commit

--- Unit3_Booleans_If_Statements/ChevyJUnitLab/Chevy.java
+++ Unit3_Booleans_If_Statements/ChevyJUnitLab/Chevy.java
@@ -1,24 +1,26 @@
 package Unit3_Booleans_If_Statements.ChevyJUnitLab;
 
 public class Chevy {
+    // instance variables
     private int year;
     private int miles;
     private double mpg;
     private int basePrice;
-    private int priceWithUpgrades = basePrice;
-    private double grandTotal = priceWithUpgrades*12;
+    private int priceWithUpgrades;
+    private double grandTotal;
     private String model;
     private String color;
     private boolean hasLuxuryPkg;
     private boolean has4WDPkg;
     private boolean hasSportsPkg;
     private final String make = "Chevrolet";
-    private final double TAX_RATE_MULT = .122;
+    private final double TAX_RATE_MULT = 1.122;
     private final double LUX_PRICE_MULT = .2;
     private final int FOUR_WHEEL_DRIVE_ADD = 3500;
     private final double SPORTS_MULT = .15;
     private final double FUEL_DEDUCTION_MULT = .8;
 
+    // full constructor
     public Chevy(int year, int miles, double mpg, int basePrice, String model,
                  String color, boolean hasLuxuryPkg, boolean has4WDPkg, boolean hasSportsPkg) {
 
@@ -32,6 +34,7 @@ public class Chevy {
         this.has4WDPkg = has4WDPkg;
         this.hasSportsPkg = hasSportsPkg;
     }
+    // default constructor
     public Chevy(){
         color = "White";
         year = 2021;
@@ -45,9 +48,12 @@ public class Chevy {
         priceWithUpgrades = 16000;
         grandTotal = 16000*TAX_RATE_MULT;
     }
+    // compareTo method compares mileage of two vehicles
     public int compareTo(Chevy other){
         return this.miles - other.miles;
     }
+    // equals method: two vehicles are considered equal if the model and color are the same or if the new/used status
+    // is the same
     public boolean equals(Chevy other){
         if (this.model.equals(other.model) && this.color.equals(other.color)){
             return true;
@@ -60,6 +66,7 @@ public class Chevy {
         }
     }
 
+    // getters and setters
     public int getYear() {
         return year;
     }
@@ -93,6 +100,7 @@ public class Chevy {
     }
 
     public int getPriceWithUpgrades() {
+        this.calcPrice();
         return priceWithUpgrades;
     }
 
@@ -101,6 +109,7 @@ public class Chevy {
     }
 
     public double getGrandTotal() {
+        this.calcPrice();
         return grandTotal;
     }
 
@@ -147,6 +156,8 @@ public class Chevy {
     public void setHasSportsPkg(boolean hasSportsPkg) {
         this.hasSportsPkg = hasSportsPkg;
     }
+
+    // toString returns full summary of vehicle
     public String toString(){
         String output = "**************************************************\n";
         output += year + " " + make + " " + model + " (" + color + ")\n";
@@ -154,15 +165,15 @@ public class Chevy {
         output += "\tMILES:\t\t\t\t" + miles + "\n";
         output += "\tFUEL EFFICIENCY:\t\t" + miles + " mpg\n";
         output += "\tPACKAGES:\n";
-        if (hasLuxuryPkg == true){
+        if (hasLuxuryPkg){
             output += "\t\t- Luxury Package";
             priceWithUpgrades += basePrice * LUX_PRICE_MULT;
         }
-        if (has4WDPkg == true) {
+        if (has4WDPkg) {
             output += "\t\t- 4WD Package";
             priceWithUpgrades += FOUR_WHEEL_DRIVE_ADD;
         }
-        if (hasSportsPkg == true) {
+        if (hasSportsPkg) {
             output += "\t\t- Sports Package";
             priceWithUpgrades += basePrice * SPORTS_MULT;
         }
@@ -170,6 +181,25 @@ public class Chevy {
             output += "\t\t- None";
         }
         output += "\n\n\tPRICE WITH UPGRADES:\t$" + (double)priceWithUpgrades + "\n";
-        output += "FINAL PRICE WITH TAX:\t$" + (double)priceWithUpgrades * TAX_RATE_MULT;
+        output += "\tFINAL PRICE WITH TAX:\t$" + (double)priceWithUpgrades * TAX_RATE_MULT + "\n";
+        output = "**************************************************";
+        return output;
+    }
+
+    // calcPrice method calculates upgrade price and total price given packages
+    public void calcPrice(){
+        priceWithUpgrades = basePrice;
+        if (hasLuxuryPkg){
+            priceWithUpgrades += basePrice * LUX_PRICE_MULT;
+        }
+        if (has4WDPkg) {
+            priceWithUpgrades += FOUR_WHEEL_DRIVE_ADD;
+        }
+        if (hasSportsPkg) {
+            priceWithUpgrades += basePrice * SPORTS_MULT;
+            mpg *= FUEL_DEDUCTION_MULT;
+        }
+        this.setPriceWithUpgrades(priceWithUpgrades);
+        grandTotal = priceWithUpgrades * TAX_RATE_MULT;
     }
 }
