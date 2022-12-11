2e073b9e85e27c0325ee04b33fd2d9a722772973
tefranke <113711340+tefranke@users.noreply.github.com>
2022-12-11 14:29:22 -0600

    commit

--- Unit6_Arrays/TriviaGame/TriviaDriver.java
+++ Unit6_Arrays/TriviaGame/TriviaDriver.java
@@ -1,10 +1,6 @@
 package Unit6_Arrays.TriviaGame;
 
-import Unit6_Arrays.CourseEx.Student;
-
-import java.io.File;
 import java.io.FileNotFoundException;
-import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class TriviaDriver {
@@ -13,14 +9,31 @@ public class TriviaDriver {
         TriviaGame myGame = new TriviaGame();
         myGame.loadFile("swimming.txt");
         Question[] qArr = myGame.getQuestionArray();
-
-
-
+        String indexes = "";
 
         System.out.println("State your name: ");
         String userName = input.nextLine();
         System.out.println("Okay " + userName + ". You will now begin a " + myGame.getTheme() + " trivia game");
 
-        System.out.println(myGame);
+        for(int i = 0; i < qArr.length; i++){
+            int index = (int)(Math.random() * qArr.length);
+            if(!indexes.contains(index + ""))
+                indexes += "" + index;
+            System.out.println(qArr[index]);
+            System.out.println("Type your answer (a, b, c, or d): ");
+            String userAns = input.nextLine();
+            if(myGame.isCorrect(userAns.toLowerCase(), qArr[index])){
+                System.out.println("\nCorrect!");
+                myGame.setTotalPoints(myGame.getTotalPoints() + qArr[index].getPointVal());
+                myGame.setNumCorrect(myGame.getNumCorrect() + 1);
+                myGame.setStreak(myGame.getStreak() + 1);
+            }
+            else{
+                System.out.println("Incorrect. The correct option was " + qArr[index].getAnswer());
+                myGame.setStreak(0);
+            }
+            myGame.setNumAnswered(myGame.getNumAnswered() + 1);
+            System.out.println(myGame.getStats(myGame.getNumAnswered() == myGame.getNumQuestions()));
+        }
     }
 }
