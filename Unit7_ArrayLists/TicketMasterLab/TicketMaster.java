f89c8d896309e92e3887be27b196743f9c278ef4
tefranke <113711340+tefranke@users.noreply.github.com>
2023-01-17 12:29:26 -0600

    kermit

--- Unit7_ArrayLists/TicketMasterLab/TicketMaster.java
+++ Unit7_ArrayLists/TicketMasterLab/TicketMaster.java
@@ -16,6 +16,10 @@ public class TicketMaster {
             String date = fileIn.next();
             double cost = fileIn.nextDouble();
             int quantity = fileIn.nextInt();
+            String temp = fileIn.nextLine();
+            int index = temp.indexOf(",");
+            String artist = temp.substring(0,index);
+            String city = temp.substring(index+2);
         }
     }
 }
