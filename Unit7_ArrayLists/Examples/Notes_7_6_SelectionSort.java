c6c870005d7acb72551f59e4ea1828c4b5a85cf1
tefranke <113711340+tefranke@users.noreply.github.com>
2023-01-25 11:58:50 -0600

    Create Notes_7_6_SelectionSort.java

---
+++ Unit7_ArrayLists/Examples/Notes_7_6_SelectionSort.java
@@ -0,0 +1,34 @@
+package Unit7_ArrayLists.Examples;
+
+import java.util.ArrayList;
+
+public class Notes_7_6_SelectionSort {
+    public static void main(String[] args) {
+        ArrayList<Integer> nums = new ArrayList<>();
+        nums.add(5);
+        nums.add(3);
+        nums.add(4);
+        nums.add(1);
+        nums.add(6);
+        nums.add(2);
+
+        selectionSort(nums);
+        System.out.println(nums);
+    }
+
+    public static void selectionSort(ArrayList<Integer> list){
+        for (int i = 0; i <= list.size() - 1; i++){
+            // look for the smallest remaining number
+            int minIndex = i;
+            for (int j = i + 1; j <= list.size(); j++){
+                if (list.get(j) < list.get(minIndex)){
+                    minIndex = j;
+                }
+            }
+            // swap the values at index i and minIndex
+            Integer temp = list.get(i);
+            list.set(i, list.get(minIndex));
+            list.set(minIndex, temp);
+        }
+    }
+}
