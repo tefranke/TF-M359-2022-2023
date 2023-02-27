be8cf946f5e52b0fc90f4820620f2c19952c5534
tefranke <113711340+tefranke@users.noreply.github.com>
2023-02-27 11:43:02 -0600

    kermit

---
+++ Unit9_Inheritance/Examples/Notes_9_2/Entree.java
@@ -0,0 +1,10 @@
+package Unit9_Inheritance.Examples.Notes_9_2;
+
+public class Entree extends MenuItem{
+    private int numSides;
+    public Entree(String name, int numSides) {
+        super(name);
+        this.numSides = numSides;
+        System.out.println("Created an Entree with " + numSides + " sides");
+    }
+}
---
+++ Unit9_Inheritance/Examples/Notes_9_2/MenuItem.java
@@ -0,0 +1,14 @@
+package Unit9_Inheritance.Examples.Notes_9_2;
+
+public class MenuItem {
+    private String name;
+
+    public MenuItem() {
+        name = "none given";
+        System.out.println("Created a MenuItem without a name?!?!?");
+    }
+    public MenuItem(String name) {
+        this.name = name;
+        System.out.println("Created a MenuItem called " + name);
+    }
+}
